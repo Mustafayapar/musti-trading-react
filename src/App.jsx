c8fc5493a12fead9475e35d9b9f38e782e8859df
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-08-07 00:21:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+ 
+import { Button } from "@/components/ui/button"
+ 
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+    <Button>Merhaba Button</Button> 
+    </>
+  )
+}
+
+export default App
