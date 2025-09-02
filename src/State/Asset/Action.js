222c80189629b3fb557a111fe281493b9b35f39e
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-09-02 23:03:33 +0300

    Asset State

---
+++ src/State/Asset/Action.js
@@ -0,0 +1,85 @@
+import api from "@/config/api";
+import * as types from "./ActionType";
+
+export const getAssetById=({assetId, jwt})=> async (dispatch)=>{
+    dispatch({type: types.GET_ASSET_REQUEST});
+
+      try {
+        const response=await api.get(
+            `/api/assets/${assetId}`,{
+                headers:{
+                Authorization:`Bearer ${jwt}`,
+                },
+            }
+        );
+        console.log("assets--", response.data)
+
+        dispatch({
+            type: types.GET_ASSET_SUCCESS,
+            payload: response.data,
+        })
+        
+    } catch (error) {
+        dispatch({
+            type:types.GET_ASSET_FAILURE,
+            error: error.message,
+        })
+        
+    }
+}
+
+
+
+export const getAssetDetails=({coinId, jwt})=> async (dispatch)=>{
+    dispatch({type: types.GET_ASSET_DETAILS_REQUEST});
+
+      try {
+        const response=await api.get(
+            `/api/assets/coin/${coinId}/user`,{
+                headers:{
+                Authorization:`Bearer ${jwt}`,
+                },
+            }
+        );
+        console.log("assets--", response.data)
+
+        dispatch({
+            type: types.GET_ASSET_DETAILS_SUCCESS,
+            payload: response.data,
+        })
+        
+    } catch (error) {
+        dispatch({
+            type:types.GET_ASSET_DETAILS_FAILURE,
+            error: error.message,
+        })
+        
+    }
+}
+
+export const getUserAssets=({jwt})=> async (dispatch)=>{
+    dispatch({type: types.GET_USER_ASSET_REQUEST});
+
+      try {
+        const response=await api.get(
+            `/api/assets`,{
+                headers:{
+                Authorization:`Bearer ${jwt}`,
+                },
+            }
+        );
+        console.log("assets--", response.data)
+
+        dispatch({
+            type: types.GET_USER_ASSET_SUCCESS,
+            payload: response.data,
+        })
+        
+    } catch (error) {
+        dispatch({
+            type:types.GET_USER_ASSET_FAILURE,
+            error: error.message,
+        })
+        
+    }
+}
