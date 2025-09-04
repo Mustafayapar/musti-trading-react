89eac94cb7b5041af96151be7add6dd73d650dce
Mustafa Yapar <97470314+Mustafayapar@users.noreply.github.com>
2025-09-04 18:20:08 +0300

    activity jsx

---
+++ src/page/Activity/Activity.jsx
@@ -0,0 +1,61 @@
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
+import { Button } from '@/components/ui/button'
+import { Table, TableBody, TableCaption,TableCell,TableHead,TableHeader, TableRow } from '@/components/ui/table'
+import { getAllOrdersForUser } from '@/State/Order/Action'
+import { calculateProfite } from '@/utils/calculateProfite'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+const Activity = () => {
+  const dispatch =useDispatch();
+  const{order}=useSelector(store=>store);
+
+  useEffect(()=>{
+    dispatch(getAllOrdersForUser({jwt: localStorage.getItem("jwt")}))
+  },[])
+  return (
+       <div className='p-5 lg:p-20 pb-4'>
+          
+              <h1 className='font-bold text-3xl pb-5'>Activity</h1>
+                  <Table className="border">
+               
+                <TableHeader>
+                  <TableRow>
+                    <TableHead className="py-5 w-[100px]">Date & Time</TableHead>
+                    <TableHead>Trading Pair</TableHead>
+                    <TableHead>Buy Price</TableHead>
+                    <TableHead>Sell Price</TableHead>
+                    <TableHead>Order Type</TableHead>
+              
+                    <TableHead className="">Profite/Loss</TableHead>
+                    <TableHead className="text-right">VALUE</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                {order.orders.map((item, index) => (
+                  <TableRow key={index}>
+                      <TableCell>
+                        <p>12.04.2020</p>
+                      <p className='text-gray-300'>12:64</p>
+                      </TableCell>
+                    <TableCell className="font-medium flex items-center gap-2">
+                      <Avatar>
+                        <AvatarImage src={item.orderItem.coin.image} />
+                      </Avatar>
+                      <span>{item.orderItem.coin.name}</span>
+                    </TableCell>
+                    <TableCell>{item.orderItem.buyPrice}</TableCell>
+                    <TableCell>{item.orderItem.sellPrice}</TableCell>
+                    <TableCell>{item.orderType}</TableCell>
+                    <TableCell>{item.orderItem.buyPrice}</TableCell>
+                    <TableCell className="" >${calculateProfite(item)}</TableCell>
+                     <TableCell className="text-right">{item.price}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+              </Table>
+            </div>
+  )
+}
+
+export default Activity
